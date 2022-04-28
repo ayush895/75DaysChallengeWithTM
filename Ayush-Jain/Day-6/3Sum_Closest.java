a5d1a4f4ab40d591cdcb4d0bf25a5dae13be2a09
ayush895 <95512102+ayush895@users.noreply.github.com>
2022-04-29 00:15:51 +0530

    Create 3Sum_Closest.java

---
+++ Ayush-Jain/Day-6/3Sum_Closest.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int closest=target>=0?Integer.MAX_VALUE:Integer.MIN_VALUE;
+        for(int i=0;i<nums.length;i++)
+        {
+            int l=i+1;
+            int r=nums.length-1;
+            while(l<r)
+            {
+                int sum=nums[i]+nums[l]+nums[r];
+                if(Math.abs(sum-target)<=Math.abs(closest-target))
+                {
+                    closest=sum;
+                }
+                 if(sum>target)
+                {
+                    r--;
+                }
+                else{
+                    l++;
+                }
+            }
+        }
+        return closest;
+    }
+}
